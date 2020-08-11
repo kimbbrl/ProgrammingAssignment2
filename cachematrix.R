a0620abb1cacab9bd81b44630372f7bf540b8720
kimbbrl <kimberly@ciencias.unam.mx>
2020-08-11 17:48:58 -0500

    Added two functions to solve a matrix and give to answer to assignment two in coursera in "cachematriz.R"

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL              #Variables that we will use to my program.
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 
 }
 
@@ -11,5 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if (!is.null(i)) {    #Condition to check if the "i" is not null an our program can run
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()     #If the before condition pass, the program return de answer
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i     ## Return a matrix that is the inverse of 'x'
 }
